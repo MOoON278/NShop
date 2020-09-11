795ec811a18bdb8fde2c6eae8500c8a5acfe4dd3
KongHaiAdam <kongha-wm19@student.tarc.edu.my>
2020-09-11 22:53:54 +0800

    knn cb firebase

---
+++ app/src/main/java/com/example/nshopmainmenu/ShoppingCart.java
@@ -0,0 +1,50 @@
+package com.example.nshopmainmenu;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+
+public class ShoppingCart extends AppCompatActivity{
+
+    TextView product_name;
+    com.google.firebase.database.DatabaseReference reff;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping_cart);
+
+        product_name = (TextView) findViewById (R.id.);
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Shopping Cart").child("1");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+                String productName = dataSnapshot.child("ProductName").getValue().toString();
+                product_name.setText(productName);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    com.google.firebase.database.DatabaseReference reff;
+
+
+    
+}
